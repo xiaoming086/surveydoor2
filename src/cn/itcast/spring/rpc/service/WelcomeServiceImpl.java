ebb32a6bfc15b97052cae2e60568fe82d1ccf8f0
xiaoming086 <1027519275@qq.com>
2015-11-19 02:33:02 +0800

    server

---
+++ src/cn/itcast/spring/rpc/service/WelcomeServiceImpl.java
@@ -0,0 +1,30 @@
+package cn.itcast.spring.rpc.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * pojo 
+ */
+public class WelcomeServiceImpl implements WelcomeService {
+	public void sayHello(String name) {
+		System.out.println(name);
+	}
+	
+	public int getLength(String name){
+		return name.length() ;
+	}
+	
+	public List<Student> getStudents(int n){
+		List<Student> list = new ArrayList<Student>();
+		Student s = null ;
+		for(int i =0 ; i < n ; i ++){
+			s = new Student();
+			s.setId(1 + i);
+			s.setName("tom" + i);
+			s.setAge(20 + i);
+			list.add(s);
+		}
+		return list ;
+	}
+}
